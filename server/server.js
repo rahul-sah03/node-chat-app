45545039ad570a29a61e3e3ed2bbd74790264102
Rahul Sah <rahulsah.eng@gmail.com>
2018-05-20 19:49:41 +0530

    Greet new users, and alert new users

--- server/server.js
+++ server/server.js
@@ -9,20 +9,33 @@ const port=process.env.PORT||3000;
 const publicPath=path.join(__dirname,'../public');
 var io=socketIO(server);
 io.on('connection',(socket)=>{
+    
     console.log('New user connected');
-   socket.emit('newMessage',{
-       from:'Rahul',
-       text:'See you then',
-       createdAt:12343
-   });
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chat app',
+        createdAt:new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user has joined',
+        createdAt:new Date().getTime()
+    });
 
     socket.on('createMessage',(message)=>{
         console.log('Create message',message);
+
+        
         io.emit('newMessage',{
             from:message.from,
             text:message.text,
             createdAt:new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createdAt:new Date().getTime()
+        // });
     });
     socket.on('disconnect',()=>{
         console.log('User was disconnected.');
