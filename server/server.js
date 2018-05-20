2971ec43e9077d8d829cc18cd9f310085cee0b78
Rahul Sah <rahulsah.eng@gmail.com>
2018-05-20 18:28:50 +0530

    Adding broadcasting events

--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ io.on('connection',(socket)=>{
 
     socket.on('createMessage',(message)=>{
         console.log('Create message',message);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        });
     });
     socket.on('disconnect',()=>{
         console.log('User was disconnected.');
