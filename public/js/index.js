b1b1fe72ba5779d45c88e1402d9c532ceebfd3d5
Rahul Sah <rahulsah.eng@gmail.com>
2018-05-24 00:38:52 +0530

    Add small modification for message text box and button

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,7 @@ jQuery('#message-form').on('submit',function(e){
         from:'User',
         text:jQuery('[name=message]').val()
     },function(){
+        jQuery('[name=message]').val('');
            });
 });
 socket.on('newMessage',function(message){
@@ -36,12 +37,15 @@ locationButton.on('click',function()
     {
         return alert('Geolocation not supported by your browser.');
     }
+    locationButton.attr('disabled','disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage',{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
         });
     },function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 })
